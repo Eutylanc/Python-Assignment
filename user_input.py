aa14a7482bcc5b191ae965840a8d2d5ab81393bd
Eutylanc <eutyla@gmail.com>
2024-10-31 20:41:19 +0300

    First commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+# Ask for user input and store the values
+name = "Eustace Ngure "
+age = "29"
+location = "Nairobi Kenya"
+
+# Print out a personalized message
+message = f"Hello my name is {name}, who is {age} years old and I live in {location}."
+print(message)
